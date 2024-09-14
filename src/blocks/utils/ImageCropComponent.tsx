4dc411b038aa7c1e88f6b39e61c8009454539f9b
Jacob Aicher <jake@anoleimaging.com>
2024-09-14 19:30:01 -0400

    Fix bottom of frame jumping to mouse position during transition from width adjustment to height adjustment

--- src/blocks/utils/ImageCropComponent.tsx
+++ src/blocks/utils/ImageCropComponent.tsx
@@ -15,38 +15,106 @@ onUpdate: (image: string) => void;
 
 const ImageCropComponent: React.FC<ImageCropComponentProps> = ({image, isFixedAspectRatio, AspectRatio, fillCropArea,maximumRatio, minimumRatio, onUpdate }) => {
     
-    const [width, setWidth] = useState(300);
-    const [height, setHeight] = useState(300);
+    // const [width, setWidth] = useState(300);
+    // const [height, setHeight] = useState(300);
     const [maxHeight, setMaxHeight] = useState(300);
-    const [clickOffset, setClickOffset] = useState(0)
+    // const [clickOffset, setClickOffset] = useState(0)
     const [verticleAdjustment, setVerticleAdjustment] = useState(false);
-    const [widthOnClick, setWidthOnClick] = useState(300);
+    // const [widthOnClick, setWidthOnClick] = useState(300);
+
+    const [resizeState, setResizeState] = useState({
+        height: 300,
+        width: 300,
+        thresholdCrossed: false, 
+        widthOnClick: 0,
+        clickOffset: 0, // the distance mouse is from the bottom of the frame
+    })
     
     const ResizeDragBounds = useRef<HTMLDivElement>(null);
 
     useEffect(() => { //sets the width of the crop window to 100% of the containing element
         if(!ResizeDragBounds.current) return;
 
-        setWidth(ResizeDragBounds.current.offsetWidth);
+        setResizeState(prevState => ({
+            ...prevState,
+            width: ResizeDragBounds.current!.offsetWidth
+
+        }))
+
+        
         setMaxHeight(ResizeDragBounds.current.offsetWidth);
 
     }, [])
+
+    useEffect(()=> { // Effect for handling action when threshold is crossed
+        
+        if(resizeState.thresholdCrossed) {
+            // width adjustment starts
+            // console.log("width adjustment")
+        }else {
+            // height adjustment resumes
+            // console.log("height adjustment resumes")
+            
+            setResizeState(prevState => ({
+                ...prevState,
+                height: maxHeight
+            }))
+
+            // setVerticleAdjustment(false)
+            
+            console.log()
+
+        }
+
+    }, [resizeState.thresholdCrossed]);
+
+    const handleThresholdCrossing = (yOffset: number, clickOffset: number) => {
+        const {thresholdCrossed, width, height} = resizeState;
+        
+        if(yOffset > maxHeight && !thresholdCrossed){ // start width adjustment
+            setResizeState(prevState => ({
+                ...prevState,
+                thresholdCrossed: true,
+                widthOnClick: width,
+            }))
+        } else if (yOffset < maxHeight && width >= maxHeight && thresholdCrossed) { //go to height adjustment
+            
+            setResizeState(prevState => ({
+                ...prevState,
+                thresholdCrossed: false,
+                widthOnClick: width,
+                height: maxHeight,
+                clickOffset: getYOffset(clickOffset)
+            }))
+        }
+    }
     
+    const getYOffset = (mousePosY: number) => {
+        
+        let yOffset = 0;
+        const target = ResizeDragBounds.current;
+        const rect = target?.getBoundingClientRect();
+
+        if (rect) {
+            yOffset = (mousePosY - rect.top) - resizeState.height;
+        }
+
+        return yOffset
+
+    }
+
     const handleMouseDown = (e: any) => {
         
-            const target = ResizeDragBounds.current;
-            const rect = target?.getBoundingClientRect();
-
-            if (rect) {
-                const initialOffsetY = (e.clientY - rect.top) - height;
-                setClickOffset(initialOffsetY)
-                setVerticleAdjustment(true);
-                setWidthOnClick(width);
-            }
-            
-            
         
+        setVerticleAdjustment(true);
+        setResizeState(prevState => ({
+            ...prevState,
+            widthOnClick: resizeState.width,
+            clickOffset: getYOffset(e.clientY)
+        }))
+     
         e.preventDefault();
+
     }
 
     const handleMouseUp = () => {
@@ -64,21 +132,32 @@ const ImageCropComponent: React.FC<ImageCropComponentProps> = ({image, isFixedAs
             if(rect){
             
                 const y = e.clientY - rect.top; // mouse position within resize dragging div
-                const yOffset = y - clickOffset;
-                
-                if(width === maxHeight) {
-                    setWidthOnClick(width);
-                setHeight(clamp(yOffset, 100, maxHeight));
-                }
-
-                if(y > maxHeight || width < maxHeight) {
-                    setWidth(clamp((widthOnClick - ((yOffset - maxHeight))),100,maxHeight))
-                    
-                }
-                
-
+                const yOffset = y - resizeState.clickOffset; // mouse position with the offset from the bottom of the frame applied
 
                 
+                
+                
+
+                setResizeState(prevState => {
+                    if(prevState.thresholdCrossed){
+                        
+                        return {
+                            ...prevState,
+                            width: clamp(prevState.widthOnClick - (yOffset - maxHeight), 100, maxHeight),
+                            
+                        }
+                    } else {
+                        
+
+                        return {
+                            ...prevState,
+                            height: clamp(yOffset, 100, maxHeight),
+                            
+                        }
+                    }
+                })
+
+                handleThresholdCrossing(yOffset, e.clientY);
             }
             
         }
@@ -99,7 +178,7 @@ const ImageCropComponent: React.FC<ImageCropComponentProps> = ({image, isFixedAs
             <div
             style={{
                 
-                width: width,
+                width: resizeState.width,
                 height: "auto",
                 margin: "0 auto"
             }}
@@ -116,7 +195,7 @@ const ImageCropComponent: React.FC<ImageCropComponentProps> = ({image, isFixedAs
                     className={'border-solid border-2 border-seam-gray-subtitle'}
                     style={{
                         width: "100%",
-                        height: height,
+                        height: resizeState.height,
                         overflow: "auto",
                         backgroundColor: "aliceblue",
                         borderRadius: "5px",
